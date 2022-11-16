a7dd914f373aefdc10b984081113e183fdbbf10c
lucasrenandns <lucasrenandns96@gmail.com>
2022-11-16 19:01:54 -0300

    Update js

--- js/matrix.js
+++ js/matrix.js
@@ -1,12 +1,18 @@
+// SELECIONANDO ELEMENTO CANVAS
 const canvas = document.getElementById("canvas")
+
+// DEFININDO CONTEXTO
 const context = canvas.getContext("2d")
 
+// DIFININDO LARGURA + ALTURA
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
+// ATRIBUINDO LARGURA + ALTURA EM VARIÁVEIS 
 const screenWidth = canvas.width
 const screenHeight = canvas.height
 
+// VARIÁVEIS/CÓDIGOS PARA EFEITO MATRIX
 const katakana = "アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン"
 const latim = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const numbers = "0123456789"
@@ -14,13 +20,14 @@ const numbers = "0123456789"
 const alphabet = katakana + latim + numbers
 
 const fontSize = 16
-const columns = Math.floor(screenWidth/fontSize)
+const columns = Math.floor(screenWidth / fontSize)
 const drops = []
 
 for(let x = 0; x < columns; x ++) {
     drops[x] = 1
 }
 
+// FUNÇÃO PARA CRIAR O ESTILO DO EFEITO MATRIX
 const contextMatrixStyle = () => {
     context.fillStyle = "rgba(0, 0, 0, 0.05)"
     context.fillRect(0, 0, screenWidth, screenHeight)
@@ -28,6 +35,7 @@ const contextMatrixStyle = () => {
     context.font = fontSize + "px sans-serif"   
 }
 
+// FUNÇÃO PARA CRIAR O EFEITO MATRIX
 const matrixEffect = () => {
     for(let i = 0; i < drops.length; i ++) {
         const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length))
@@ -40,4 +48,5 @@ const matrixEffect = () => {
     }
     contextMatrixStyle()
 }
+// CHAMANDO A FUNÇÃO EFEITO MATRIX NO DOCUMENTO
 setInterval(matrixEffect, 30)
